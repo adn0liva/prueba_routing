fde4d3ddb63ffec36feea2b3a6c4b1ac3a57072a
adan oliva <a.oliva.navea@gmail.com>
2020-12-20 17:03:33 -0300

    Se agrega tipo de carga como tabla de dominio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_195829) do
+ActiveRecord::Schema.define(version: 2020_12_20_200253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2020_12_20_195829) do
     t.string "name", null: false
     t.string "phone"
     t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "load_types", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
