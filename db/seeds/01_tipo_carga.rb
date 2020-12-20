ee18484c65843e86f8550a4eb8c127a6cd8b4cf9
adan oliva <a.oliva.navea@gmail.com>
2020-12-20 17:25:04 -0300

    Se cargan datos en tabla de dominio tipo de carga

---
+++ db/seeds/01_tipo_carga.rb
@@ -0,0 +1,9 @@
+# cargamos los tipos de carga, les entregamos el id para poder usar constantes en el modelo
+tipos_de_carga = [
+  {id: 1, name: 'General'},
+  {id: 2, name: 'Refrigerada'}
+]
+tipos_de_carga.each do |tc_hash|
+  tc_objeto = LoadType.find_or_create_by(id: tc_hash[:id])
+  tc_objeto.update(name: tc_hash[:name])
+end
