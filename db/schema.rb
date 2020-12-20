38e3d1a94509e846e51985e4a1c6b5866cc57c19
adan oliva <a.oliva.navea@gmail.com>
2020-12-20 18:34:32 -0300

    Se agregan campos a tabla conductores

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_201047) do
+ActiveRecord::Schema.define(version: 2020_12_20_212848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_12_20_201047) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "vehicle_id"
+    t.string "cities", array: true
+    t.integer "max_stops_accepted"
     t.index ["vehicle_id"], name: "index_drivers_on_vehicle_id"
   end
 
