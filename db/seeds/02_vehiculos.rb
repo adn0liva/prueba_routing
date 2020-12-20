6231a6b24c753850387072f61a12bdafb744d4ee
adan oliva <a.oliva.navea@gmail.com>
2020-12-20 18:19:34 -0300

    Se cambia capacidad de vehiculos, dejando cercana al maximo en rutas

--- db/seeds/02_vehiculos.rb
+++ db/seeds/02_vehiculos.rb
@@ -1,11 +1,11 @@
 # agregamos vehiculos base para realizar pruebas
 vehiculos = [
-  { id: 1, capacity: 25, load_type_id: LoadType::TC_GENERAL_ID },
-  { id: 2, capacity: 60, load_type_id: LoadType::TC_GENERAL_ID },
-  { id: 3, capacity: 200, load_type_id: LoadType::TC_GENERAL_ID },
-  { id: 4, capacity: 25, load_type_id: LoadType::TC_REFRIGERADA_ID },
-  { id: 5, capacity: 60, load_type_id: LoadType::TC_REFRIGERADA_ID },
-  { id: 6, capacity: 200, load_type_id: LoadType::TC_REFRIGERADA_ID }
+  { id: 1, capacity: 10, load_type_id: LoadType::TC_GENERAL_ID },
+  { id: 2, capacity: 30, load_type_id: LoadType::TC_GENERAL_ID },
+  { id: 3, capacity: 60, load_type_id: LoadType::TC_GENERAL_ID },
+  { id: 4, capacity: 10, load_type_id: LoadType::TC_REFRIGERADA_ID },
+  { id: 5, capacity: 30, load_type_id: LoadType::TC_REFRIGERADA_ID },
+  { id: 6, capacity: 60, load_type_id: LoadType::TC_REFRIGERADA_ID }
 ]
 vehiculos.each do |v_hash|
   v_objecto = Vehicle.find_or_create_by(id: v_hash[:id])
