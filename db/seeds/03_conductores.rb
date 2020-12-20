36fe4744cc742f194d037cfec993dcc198609df3
adan oliva <a.oliva.navea@gmail.com>
2020-12-20 18:42:02 -0300

    Se modifican los datos base de conductores y rutas
    se agregan las comunas y los maximos de paradas a los conductores

--- db/seeds/03_conductores.rb
+++ db/seeds/03_conductores.rb
@@ -1,11 +1,18 @@
+# definimos variables bases
+comunas_centro = ['Santiago','Estacion central', 'Providencia']
+comunas_oriente = ['Las condes','Vitacura', 'Lo barnechea']
+comunas_poniente = ['Maipu','Pudahuel','Lo prado']
 # cargamos conductores base para el sistema
 conductores = [
-  {id: 1, name: 'Juan Perez', phone: '+5691111111', email: 'juperez@conductores.cl'},
-  {id: 2, name: 'Jose Perez', phone: '+5691111112', email: 'joperez@conductores.cl'},
-  {id: 3, name: 'Luis Perez', phone: '+5691111113', email: 'luperez@conductores.cl', vehicle_id: 3},
-  {id: 4, name: 'Rodrigo Perez', phone: '+5691111114', email: 'roperez@conductores.cl'},
-  {id: 5, name: 'Carlos Perez', phone: '+5691111115', email: 'caperez@conductores.cl'},
-  {id: 6, name: 'Matias Perez', phone: '+5691111116', email: 'maperez@conductores.cl', vehicle_id: 6}
+  {id: 1, name: 'Juan Perez', phone: '+5691111111', email: 'juperez@conductores.cl', cities: comunas_centro, max_stops_accepted: 15},
+  {id: 2, name: 'Jose Perez', phone: '+5691111112', email: 'joperez@conductores.cl', cities: comunas_oriente, max_stops_accepted: 30},
+  {id: 3, name: 'Luis Perez', phone: '+5691111113', email: 'luperez@conductores.cl', vehicle_id: 3, cities: comunas_poniente, max_stops_accepted: 60},
+  {id: 4, name: 'Rodrigo Perez', phone: '+5691111114', email: 'roperez@conductores.cl', cities: comunas_centro, max_stops_accepted: 15},
+  {id: 5, name: 'Carlos Perez', phone: '+5691111115', email: 'caperez@conductores.cl', cities: comunas_oriente, max_stops_accepted: 30},
+  {id: 6, name: 'Matias Perez', phone: '+5691111116', email: 'maperez@conductores.cl', vehicle_id: 6, cities: comunas_poniente, max_stops_accepted: 60},
+  # estos 2 no tienen maximo de paradas
+  {id: 7, name: 'Pablo Perez', phone: '+5691111117', email: 'paperez@conductores.cl', cities: comunas_oriente},
+  {id: 8, name: 'Esteban Perez', phone: '+5691111118', email: 'esperez@conductores.cl', cities: comunas_poniente}
 ]
 conductores.each do |c_hash|
   d_objeto = Driver.find_or_initialize_by(id: c_hash[:id])
--- db/seeds/04_rutas.rb
+++ db/seeds/04_rutas.rb
@@ -1,28 +1,32 @@
+# definimos variables bases
 hora_base =  Time.parse('2020-12-21 08:00')
+comunas_centro = ['Santiago','Estacion central', 'Providencia']
+comunas_oriente = ['Las condes','Vitacura', 'Lo barnechea']
+comunas_poniente = ['Maipu','Pudahuel','Lo prado']
 rutas = [
-  { id: 1, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 8, cities: ['Santiago','Estacion central', 'Providencia'], stops_amount: 8 },
-  { id: 2, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 8, cities: ['Santiago','Estacion central', 'Providencia'], stops_amount: 8 },
-  { id: 3, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 8, cities: ['Santiago','Estacion central', 'Providencia'], stops_amount: 8 },
+  { id: 1, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 8, cities: comunas_centro, stops_amount: 8 },
+  { id: 2, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 8, cities: comunas_centro, stops_amount: 8 },
+  { id: 3, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 8, cities: comunas_centro, stops_amount: 8 },
   
-  { id: 4, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 28, cities: ['Las condes','Vitacura', 'Lo barnechea'], stops_amount: 25 },
-  { id: 5, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 28, cities: ['Las condes','Vitacura', 'Lo barnechea'], stops_amount: 25 },
-  { id: 6, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 28, cities: ['Las condes','Vitacura', 'Lo barnechea'], stops_amount: 25 },
+  { id: 4, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 28, cities: comunas_oriente, stops_amount: 25 },
+  { id: 5, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 28, cities: comunas_oriente, stops_amount: 25 },
+  { id: 6, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 28, cities: comunas_oriente, stops_amount: 25 },
   
-  { id: 7, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 58, cities: ['Maipu','Pudahuel','Lo prado'], stops_amount: 50 },
-  { id: 8, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 58, cities: ['Maipu','Pudahuel','Lo prado'], stops_amount: 50 },
-  { id: 9, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 58, cities: ['Maipu','Pudahuel','Lo prado'], stops_amount: 50 },
+  { id: 7, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 58, cities: comunas_poniente, stops_amount: 50 },
+  { id: 8, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 58, cities: comunas_poniente, stops_amount: 50 },
+  { id: 9, load_type_id: LoadType::TC_GENERAL_ID, load_sum: 58, cities: comunas_poniente, stops_amount: 50 },
   
-  { id: 10, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 8, cities: ['Santiago','Estacion central', 'Providencia'], stops_amount: 8 },
-  { id: 11, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 8, cities: ['Santiago','Estacion central', 'Providencia'], stops_amount: 8 },
-  { id: 12, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 8, cities: ['Santiago','Estacion central', 'Providencia'], stops_amount: 8 },
+  { id: 10, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 8, cities: comunas_centro, stops_amount: 8 },
+  { id: 11, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 8, cities: comunas_centro, stops_amount: 8 },
+  { id: 12, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 8, cities: comunas_centro, stops_amount: 8 },
   
-  { id: 13, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 28, cities: ['Las condes','Vitacura', 'Lo barnechea'], stops_amount: 25 },
-  { id: 14, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 28, cities: ['Las condes','Vitacura', 'Lo barnechea'], stops_amount: 25 },
-  { id: 15, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 28, cities: ['Las condes','Vitacura', 'Lo barnechea'], stops_amount: 25 },
+  { id: 13, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 28, cities: comunas_oriente, stops_amount: 25 },
+  { id: 14, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 28, cities: comunas_oriente, stops_amount: 25 },
+  { id: 15, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 28, cities: comunas_oriente, stops_amount: 25 },
   
-  { id: 16, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 58, cities: ['Maipu','Pudahuel','Lo prado'], stops_amount: 50 },
-  { id: 17, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 58, cities: ['Maipu','Pudahuel','Lo prado'], stops_amount: 50 },
-  { id: 18, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 58, cities: ['Maipu','Pudahuel','Lo prado'], stops_amount: 50 },
+  { id: 16, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 58, cities: comunas_poniente, stops_amount: 50 },
+  { id: 17, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 58, cities: comunas_poniente, stops_amount: 50 },
+  { id: 18, load_type_id: LoadType::TC_REFRIGERADA_ID, load_sum: 58, cities: comunas_poniente, stops_amount: 50 },
 ]
 contador = 0
 rutas.each_with_index do |r_hash, index|
