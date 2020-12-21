43980e13f2ba86715c1ffb8c08093f812e2a4b0b
adan oliva <a.oliva.navea@gmail.com>
2020-12-21 02:36:06 -0300

    Se modifican modelos para agregar metodos que permiten identificar y filtrar busquedas

--- app/models/route.rb
+++ app/models/route.rb
@@ -7,6 +7,10 @@ class Route < ApplicationRecord
   scope :asignadas, -> { where(vehicle_id: !nil, driver_id: !nil) }
   scope :carga_general, -> { where(load_type_id: LoadType::TC_GENERAL_ID) }
   scope :carga_refrigerada, -> { where(load_type_id: LoadType::TC_REFRIGERADA_ID) }
+  scope :por_dia, ->(fecha) { 
+    filtro_fecha = { inicio_dia: fecha.beginning_of_day, fin_dia: fecha.end_of_day }
+    where('starts_at >= :inicio_dia AND ends_at <= :fin_dia ',filtro_fecha)
+  }
   
   def asignada?
     self.vehicle_id.present? && self.driver_id.present?
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,4 +3,23 @@ class Vehicle < ApplicationRecord
   belongs_to :driver, optional: true
   has_many :routes
 
+  # devuelve los vehiculos con ruta hoy
+  scope :rutas_por_dia, ->(fecha) { 
+    filtro_fecha = { inicio_dia: fecha.beginning_of_day, fin_dia: fecha.end_of_day }
+    includes(:routes)
+    .where('routes.starts_at >= :inicio_dia AND routes.ends_at <= :fin_dia ', filtro_fecha)
+  }
+  # devuelve vehiculos sin rutas para fecha seleccionada
+  scope :sin_rutas_asignadas, ->(fecha) { 
+    id_con_ruta_hoy = rutas_por_dia(fecha).pluck(:id)
+    where.not(id: id_con_ruta_hoy)
+  }
+  # devuelve vehiculos del tipo de carga recibido y con la capacidad necesaria
+  scope :por_tipo_y_carga_maxima, ->(tipo_carga, carga_maxima) { 
+    where(load_type_id: tipo_carga).where('capacity >= :carga_maxima', carga_maxima: carga_maxima)
+  }
+
+  def tiene_conductor?
+    self.driver_id.present?
+  end
 end
